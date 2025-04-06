7291e95c4e00a8e59dbbc90b642d2e222b491797
Michał Burzyński <michal.burzynski0805@gmail.com>
2025-04-06 11:17:34 +0200

    Fixed chart overflow

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,25 +1,36 @@
 document.getElementById('uploadForm').onsubmit = async function(e) {
-    e.preventDefault();
-    const formData = new FormData(this);
-    const fileInput = document.getElementById('fileInput');
-    const fileName = fileInput.files[0].name;
-    try {
-      const res = await fetch('/upload', { method: 'POST', body: formData });
-      const data = await res.json();
-      const trace = {
-        x: data.map(p => p.x),
-        y: data.map(p => p.y),
-        text: data.map(p => `PMID: ${p.pmid}<br>GSE: ${p.gse_id}<br>${p.title}`),
-        mode: 'markers',
-        marker: { size: 12, color: data.map(p => p.label), colorscale: 'Viridis' },
-        type: 'scatter'
-      };
-      const layout = {
-        title: `Chart for ${fileName}`,
-        margin: { t: 50 }
-      };
-      Plotly.newPlot('plot', [trace], layout);
-    } catch (err) {
-      console.error('Error during file upload or plotting:', err);
-    }
+  e.preventDefault()
+  const formData = new FormData(this);
+  const fileName = document.getElementById('fileInput').files[0].name;
+
+  try {
+    const res = await fetch('/upload', {method: 'POST', body: formData});
+    const data = await res.json();
+
+    const trace = {
+      x: data.map(p => p.x),
+      y: data.map(p => p.y),
+      text: data.map(p => `PMID: ${p.pmid}<br>GSE: ${p.gse_id}<br>${p.title}`),
+      mode: 'markers',
+      marker: { 
+        size: 10,
+        color: data.map(p => p.label),
+        colorscale: 'Viridis'
+      },
+      type: 'scatter'
+    };
+
+    const layout = {
+      title: `Chart for ${fileName}`,
+      autosize: true,
+      margin: {
+        t: 50
+      }
+    };
+
+    const config = { responsive: true };
+    Plotly.newPlot('plot', [trace], layout, config);
+  } catch (err) {
+    console.error('Error during file upload or plotting:', err);
   }
+}
